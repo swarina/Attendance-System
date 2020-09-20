16403d50955f6b06c0ae4be4a416d5887a96625e
Swarina <swarinajaiswal@gmail.com>
2020-09-20 14:29:49 +0530

    Updated

--- application.py
+++ application.py
@@ -13,6 +13,7 @@ print("2. Add a student")
 print("3. Train the model")
 print("4. Scan face for attendance")
 print("5. Check Attendance")
+print("6. View Database")
 to_do = int(input("\n Enter Valid number: "))
 
 if to_do == 1:
@@ -23,17 +24,22 @@ elif to_do == 3:
 	train.train()
 elif to_do == 4:
 	recognition.recognise()
-else:
+elif to_do == 5:
 	conn = sqlite3.connect('database.db')
 	cur = conn.cursor()
 	roll_no = input("\nEnter Roll no. :")
-	# cur.execute("SELECT * FROM attendance")
-	# items = cur.fetchall()
-	# for _ in items:
-    # 		print(_)
 	cur.execute("SELECT date FROM attendance WHERE _{s_roll_no}=1".format(s_roll_no=roll_no))
 	# print(cur.fetchall())
 	presents = len(cur.fetchall())
 	cur.execute("SELECT * FROM attendance")
 	total = len(cur.fetchall())
 	print(str(presents) + '/' + str(total))
+else:
+	conn = sqlite3.connect('database.db')
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM attendance")
+	items = cur.fetchall()
+	for _ in items:
+    		print(_)
+
+    	
